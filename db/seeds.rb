d7868508f6b82501ada43701a81c6bc0f3bc0188
Georg Donner <georgdonner98@gmail.com>
2017-11-01 10:45:35 +0100

    S001: Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Lecturer.destroy_all()
+Subject.destroy_all()
+Course.destroy_all()
+
+Lecturer.create(title: 'Prof. Dr.', name: 'Juliane Fink', email: 'jule@fink.de')
+Subject.create(name: 'React Fundamentals', study_path: 'IMI-B')
+Course.create(description: 'Learn the fundamentals of React', subject: Subject.first, lecturer: Lecturer.first)
