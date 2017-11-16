b590dcf2b6e427bdcb898be5adb90faf9d949936
JuliusDeckert <s0545190@htw-berlin.de>
2017-11-16 10:54:34 +0100

    A2: Whitelisting

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+
 
   # GET /courses
   # GET /courses.json
--- app/controllers/lecturers_controller.rb
+++ app/controllers/lecturers_controller.rb
@@ -1,6 +1,5 @@
 class LecturersController < ApplicationController
-  before_action :set_lecturer, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+
 
   # GET /lecturers
   # GET /lecturers.json
