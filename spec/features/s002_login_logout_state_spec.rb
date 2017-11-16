b2a21a66946c3a6a5ffc3ffee266efb7512dae42
Georg Donner <georgdonner98@gmail.com>
2017-11-16 12:46:34 +0100

    A2-rspec: Fix login status tests.

--- spec/features/s002_login_logout_state_spec.rb
+++ spec/features/s002_login_logout_state_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-describe 'S002: Change login/logout buttons depending on user login status' do
+describe 'S002: Login status' do
   before :each do
     user = User.create(email: 'jule@fink.de', password: 'geheim', password_confirmation: 'geheim')
     login_as(user, :scope => :user)
@@ -8,15 +8,15 @@ describe 'S002: Change login/logout buttons depending on user login status' do
   it 'changes login button to logout after login' do
     visit root_path
 
-    expect(page).to have_content('Logout')
-    expect(page).to have_no_content('Login')
+    expect(page).to have_selector("input[type=submit][value='Logout']")
+    expect(page).to have_no_selector("input[type=submit][value='Login']")
   end
-
+  
   it 'changes logout button to login after logout' do
-    visit root_path
     logout(:user)
-
-    expect(page).to have_content('Login')
-    expect(page).to have_no_content('Logout')
+    visit root_path
+    
+    expect(page).to have_selector("input[type=submit][value='Login']")
+    expect(page).to have_no_selector("input[type=submit][value='Logout']")
   end
 end
