d2f8883b9d3dc8d260fab6b9478a23c48c2077d7
Georg Donner <georgdonner98@gmail.com>
2017-12-06 19:59:32 +0100

    Fix subject search only returning results if subject name starts with keyword(s).

--- app/models/subject_search_term.rb
+++ app/models/subject_search_term.rb
@@ -10,12 +10,12 @@ class SubjectSearchTerm < ApplicationRecord
   private
     def build_for_name_search(search_term)
       @where_clause << case_insensitive_search(:name)
-      @where_args[:name] = starts_with(search_term)
+      @where_args[:name] = contains(search_term)
       @order = "name asc"
     end
 
-    def starts_with(search_term)
-      search_term + "%"
+    def contains(search_term)
+      "%" + search_term + "%"
     end
 
     def case_insensitive_search(field_name)
