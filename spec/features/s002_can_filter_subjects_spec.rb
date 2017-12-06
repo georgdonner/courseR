e906bff010d2cd47f141bcd672062b765efe8785
Georg Donner <georgdonner98@gmail.com>
2017-12-06 17:47:47 +0100

    A3-rspec: Fix subject filtering test.

--- spec/features/s002_can_filter_subjects_spec.rb
+++ spec/features/s002_can_filter_subjects_spec.rb
@@ -7,7 +7,7 @@ describe 'S002: User can filter subjects by semester' do
   it 'links to specific subject overview' do
     visit subjects_path
 
-    click_button('Semester 1')
+    click_link('Semester 1')
     
     expect(page).to have_content('Fun with Angular')
     expect(page).to have_no_content('Fun with React')
